7d96c831cf360b3fba8fe365298216943c969e75
AwlsomeAlex <awlsomeabc1@outlook.com>
2017-02-16 18:59:19 -0500

    Added AwlsomeLinux Generation Script & Library

---
+++ changelog.md
@@ -0,0 +1,5 @@
+# AwlsomeLinux Changelog
+
+--- Update 02/16/17 (v0.0.1a) ---
+* Created AwlsomeLinux Generation Script (Complete)
+* Created AwlsomeLinux Generation Library [check_dialog]
---
+++ src/awlsomelinux.sh
@@ -0,0 +1,65 @@
+#!/bin/bash 
+
+##################################
+#                                #
+# AwlsomeLinux Generation Script #
+#                                #
+##################################
+#  Made by AwlsomeAlex (GPLv3)   #
+##################################
+
+
+#############################
+# Library Retrival Function # get_library
+#######################################
+# If the library isn't found, it will #
+# automatically be downloaded from    #
+# the AwlsomeLinux Repository         #
+#######################################
+get_library()
+{
+	echo ""
+	echo "##########################"
+	echo "# Downloading Library... #"
+	echo "##########################"
+	echo ""
+	#wget https://github.com/awlsomealex/awlsomelinux/ ## TO BE ADDED
+	echo ""
+	echo "######################"
+	echo "# Download Complete! #"
+	echo "######################"
+	echo "Please Restart AwlsomeLinux Generation Script to Continue!"
+	exit 1
+
+
+##########################
+# Pre-Execution Commands #
+###############################
+# Certain Commands to confirm #
+# AwlsomeLinux will work at   #
+# the minimum will be checked #
+###############################
+
+## Call 'awlsomelinux.lib' to load rest of functions ##
+. ./awlsomelinux.lib
+
+if [ -f "awlsomelinux.lib" ]
+	then
+		echo "The AwlsomeLinux Generation Library has been loaded!"
+	else
+		echo "The AwlsomeLinux Generation Library has not been found."
+		get_library
+fi
+
+## Check forthe program 'dialog' ##
+check_dialog
+
+
+#############
+# Main Menu #
+##########################
+# The Main Menu function #
+# will be loaded from    #
+# th external library    #
+##########################
+#main_menu
